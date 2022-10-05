0d6347f1025073dbd435cba4da956b53f212c2b6
IanMwaniki <ian.mwaniki@student.moringaschool.com>
2022-10-05 14:06:55 +0300

    Assignment complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,60 +1,68 @@
+// import React, {useState} from "react";
 
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
 
-import React, { useState } from "react";
+// function handleFirstNameChange(event) {
+//   setFirstName(event.target.value);
+// }
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-  const [submittedData, setSubmittedData] = useState([]);
-
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = { firstName: firstName, lastName: lastName };
-    const dataArray = [...submittedData, formData];
-    setSubmittedData(dataArray);
-    setFirstName("");
-    setLastName("");
-  }
-
-  const listOfSubmissions = submittedData.map((data, index) => {
-    return (
-      <div key={index}>
-        {data.firstName} {data.lastName}
-      </div>
-    );
-  });
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleFirstNameChange} value={firstName} />
-        <input type="text" onChange={handleLastNameChange} value={lastName} />
-        <button type="submit">Submit</button>
-      </form>
-      <h3>Submissions</h3>
-      {listOfSubmissions}
-    </div>
-  );
-}
+// function handleLastNameChange(event) {
+//   setLastName(event.target.value);
+// }
 
 //   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <input type="text" onChange={handleFirstNameChange} value={firstName} />
-//         <input type="text" onChange={handleLastNameChange} value={lastName} />
-//         <button type="submit">Submit</button>
-//       </form>
-//       <h3>Submissions</h3>
-//       {listOfSubmissions}
-//     </div>
+//     <form>
+//       <input type="text" onChange={handleFirstNameChange} value={firstName} />
+//       <input type="text" onChange={handleLastNameChange} value={lastName} />
+//       <button type="submit">Submit</button>
+//     </form>
 //   );
 // }
+
+// export default Form;
+
+
+// import React, {useState} from "react";
+
+// function Form() {
+//   const [newsletter, setNewsletter] = useState(false);
+
+//   function handleNewsletterChange(event) {
+//     // .checked, not .value!
+//     setNewsletter(event.target.checked);
+//   }
+
+//   return (
+//     <form>
+//       <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+//       <input type="checkbox" id="newsletter" onChange={handleNewsletterChange} checked={newsletter}/>
+//       <button type="submit">Submit</button>
+//     </form>
+//   )
+// }
+
+// export default Form;
+
+import React from "react";
+
+function Form(props) {
+  return (
+    <form>
+    <input
+      type="text"
+      onChange={props.handleFirstNameChange}
+      value={props.firstName}
+    />
+    <input
+      type="text"
+      onChange={props.handleLastNameChange}
+      value={props.lastName}
+    />
+    <button type="submit">Submit</button>
+  </form>
+  );
+}
 
 export default Form;
